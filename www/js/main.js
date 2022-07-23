52999c665a53d57c85aa22a03f6f2dfb0d6adc8f
bigreni <santhanakrishnan.sai@gmail.com>
2022-07-23 01:46:04 -0500

    2.0.1

--- www/js/main.js
+++ www/js/main.js
@@ -59,7 +59,7 @@
 
     function loadInterstitial() {
         if ((/(android|windows phone)/i.test(navigator.userAgent))) {
-            AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: false, autoShow: true });
+            AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: false, autoShow: false });
             //document.getElementById("screen").style.display = 'none';     
         } else if ((/(ipad|iphone|ipod)/i.test(navigator.userAgent))) {
             AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: false, autoShow: true });
